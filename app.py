adbeb543f3c251e0a56626bea68d12ec933b4623
wzeng-22 <98065036+wzeng-22@users.noreply.github.com>
2022-08-12 19:05:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ import plotly.express as px
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
 import dash
-from jupyter_dash import JupyterDash
+
+import dash-renderer
+import gunicorn
+
+#from jupyter_dash import JupyterDash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
